576d8cd4667d177b6e18e742ead0be49d729decc
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-14 20:50:38 +0200

    T4.2 index.js käyttää nyt logger.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
 const blogSchema = mongoose.Schema({
   title: String,
@@ -46,5 +47,5 @@ app.post('/api/blogs', (request, response) => {
 //const PORT = 3003
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
