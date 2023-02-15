cd18ccea9d4688271b8a51d4e2c437c1f98837eb
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-15 18:14:07 +0200

    T4.4 Lisätään tyhjän blogilistan testi

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -12,6 +12,11 @@ describe('total likes', () => {
         }
     ]
 
+    test('of empty list is zero', () => {
+        const result = listHelper.totalLikes([])
+        expect(result).toBe(0)
+    })
+
     test('when list has only one blog equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
