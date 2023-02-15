f4d76b99d131db7d1aee82100734dd3a986542a7
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-15 18:12:29 +0200

    T4.4 aloitetaan tehtävä

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+
+const totalLikes = (blogs) => {
+    const result = blogs.reduce((like_sum,blogi) => like_sum + blogi.likes,0)
+    return result
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
