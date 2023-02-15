a0159a1bfdb7b06a812787557820b91c58b065ac
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-02-15 18:16:35 +0200

    T4.4 lisätään oma testi blogit ilman likejä

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -12,6 +12,33 @@ describe('total likes', () => {
         }
     ]
 
+    const listWithBlogsWithoutLikes = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 0,
+            __v: 0
+        }
+    ]
+
     test('of empty list is zero', () => {
         const result = listHelper.totalLikes([])
         expect(result).toBe(0)
@@ -21,4 +48,10 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
     })
+
+    test('when blogs in the list do not have any likes then there are zero likes', () => {
+        const result = listHelper.totalLikes(listWithBlogsWithoutLikes)
+        expect(result).toBe(0)
+    })
+
 })
