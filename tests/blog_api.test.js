a3eaf182662dcde4ffddaf7d7ed61ff4b8f2ed6a
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-09 09:39:33 +0300

    T4.8: refaktoroidaan testitietokannan alustus

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -9,10 +9,14 @@ const Blog = require('../models/blog')
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    let blogObject = new Blog(helper.initialBlogs[0])
-    await blogObject.save()
-    blogObject = new Blog(helper.initialBlogs[1])
-    await blogObject.save()
+
+    const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogObjects.map(blog => blog.save())
+    await Promise.all(promiseArray)
+    // let blogObject = new Blog(helper.initialBlogs[0])
+    // await blogObject.save()
+    // blogObject = new Blog(helper.initialBlogs[1])
+    // await blogObject.save()
 })
 
 test('blogit palautetaan json muodossa', async () => {
