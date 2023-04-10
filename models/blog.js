cf3bf0aab3cd9635a8672f249787d17d8e2c0ecb
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-10 12:43:19 +0300

    T4.11* Lisätään blogin likes-kentälle 'default'

--- models/blog.js
+++ models/blog.js
@@ -3,12 +3,20 @@ const config = require('../utils/config')
 
 mongoose.set('strictQuery',false)
 
+// T4.11*
+// Käytetään Mongoosen default
+//https://mongoosejs.com/docs/defaults.html
+// Note: Mongoose only applies a default if the value of the path
+// is strictly undefined.
 
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes: {
+      type: Number,
+      default: 0
+    } 
   })
 
 blogSchema.set('toJSON', {
