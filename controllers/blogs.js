1b87775aca79f3b89105540f0a0883dd2a11415e
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-10 11:55:08 +0300

    T4.10: blogin kontrolleri käyttää nyt async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -7,14 +7,17 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+    const savedBLog = await blog.save()
+    response.status(201).json(savedBLog)
+
+    // blog
+    //     .save()
+    //     .then(result => {
+    //         response.status(201).json(result)
+    //     })
 })
 
 module.exports = blogsRouter
