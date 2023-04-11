a6213476a61131cf8b501c8773503997f53eed10
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-11 11:34:32 +0300

    T4.14* lisätään PUT ja VSCRestClient testejä

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -19,4 +19,25 @@ blogsRouter.delete('/:id', async (request, response) => {
     response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    console.log(request.body)
+    console.log(body)
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    console.log(blog)
+
+    // tälle annetaan javascript-olio eikä Blog-olio
+    // new parametri palauttaa muuttuneen olion kutsujalle
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+})
+
 module.exports = blogsRouter
