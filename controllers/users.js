f27945b3921144b70561c9d9bb990a476847e22b
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-13 15:17:17 +0300

    T4.17 users.js populate vain tietyt kentät

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,8 @@ const User = require('../models/user')
 const logger = require('../utils/logger')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('blogs')
+    const users = await User.find({})
+        .populate('blogs', { url: 1, title: 1, author: 1 })
 
     response.json(users)
 })
