ca5571dd0909fd329988ca8ce006e212250f874a
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-13 15:58:46 +0300

    T4.18 login ja tokenin palautus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const usersRouter = require('./controllers/users')
 //const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 
 mongoose.set('strictQuery',false)
@@ -19,6 +20,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 
 // https://mongoosejs.com/docs/validation.html#validation-errors
