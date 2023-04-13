d08e149628e0fce6feeae3d4f775f3cc1c6b8c97
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-13 16:16:31 +0300

    T4.19 valmis eli blogin post vain kirjautuneena

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
@@ -12,16 +22,25 @@ blogsRouter.post('/', async (request, response) => {
     //const blog = new Blog(request.body)
     const body = request.body
     // tilapäinen kovakoodattu userId T4.17
-    if (!body.userId) {
-        const dummyUsers = await User.find({})
-        //console.log(dummyUsers)
-        const dummyUserId = dummyUsers[0]._id.toString()
-        //console.log(dummyUserId)
-        body.userId = dummyUserId
+    // if (!body.userId) {
+    //     const dummyUsers = await User.find({})
+    //     //console.log(dummyUsers)
+    //     const dummyUserId = dummyUsers[0]._id.toString()
+    //     //console.log(dummyUserId)
+    //     body.userId = dummyUserId
+    // }
+    //console.log(request)
+    //console.log(body)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    
+    //console.log(decodedToken)
+    
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
     }
     
-    
-    const user = await User.findById(body.userId)
+    //const user = await User.findById(body.userId)
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
