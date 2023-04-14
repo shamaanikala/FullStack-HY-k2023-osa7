65dd988fa4e1bba3c2d33b8f9c93c410d34fc593
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-14 15:25:43 +0300

    T4.20* siirretään errorHandler -> middleware.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 
 
 mongoose.set('strictQuery',false)
@@ -23,43 +24,43 @@ app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 
-// https://mongoosejs.com/docs/validation.html#validation-errors
-const errorHandler = (error,request,response,next) => {
-    logger.error(error.message)
-    //logger.error(error)
-    if (error.name === 'CastError') { // PUT
-        return response.status(400).send({ error: `Error while updating likes field: likes must be a number`})
-    }
-    else if (error.name === 'ValidationError') {
-        //console.log(error.errors)
-        if (error.errors.likes) {
-            //logger.error(error.errors.likes.name) // CastError
-            logger.error(error.errors.likes.message)
-            return response.status(400).send({error: `likes field must be a number`})
-        } else if (error.errors.url) {
-            logger.error(error.errors.url.message)
-            return response.status(400).send({error: `url field can't be empty`})
-        } else if (error.errors.title) {
-            logger.error(error.errors.title.message)
-            return response.status(400).send({error: `title field can't be empty`})
-        } else if (error.errors.username.message === 'Username must have at least 3 characters') {
-            logger.error(error.errors.username.message)
-            return response.status(400).send({ error: `Username must have at least 3 characters (given username was: '${error.errors.username.value}')`})
-        }
-        //console.log(`Required fields missing ${Object.keys(error.errors)}`)
-        //return response.status(400).send({error: `Required fields missing ${Object.keys(error.errors)}`})
-        return response.status(400).json({ error: error.message })
-    } else if (error.name === 'JsonWebTokenError') {
-        if (error.message === 'jwt must be provided') {
-            logger.error(`Error: ${error.name}: ${error.message}`)
-            return response.status(400).send({ error: 'The token must be provided in the header.' })
-        }
-    }
+// // https://mongoosejs.com/docs/validation.html#validation-errors
+// const errorHandler = (error,request,response,next) => {
+//     logger.error(error.message)
+//     //logger.error(error)
+//     if (error.name === 'CastError') { // PUT
+//         return response.status(400).send({ error: `Error while updating likes field: likes must be a number`})
+//     }
+//     else if (error.name === 'ValidationError') {
+//         //console.log(error.errors)
+//         if (error.errors.likes) {
+//             //logger.error(error.errors.likes.name) // CastError
+//             logger.error(error.errors.likes.message)
+//             return response.status(400).send({error: `likes field must be a number`})
+//         } else if (error.errors.url) {
+//             logger.error(error.errors.url.message)
+//             return response.status(400).send({error: `url field can't be empty`})
+//         } else if (error.errors.title) {
+//             logger.error(error.errors.title.message)
+//             return response.status(400).send({error: `title field can't be empty`})
+//         } else if (error.errors.username.message === 'Username must have at least 3 characters') {
+//             logger.error(error.errors.username.message)
+//             return response.status(400).send({ error: `Username must have at least 3 characters (given username was: '${error.errors.username.value}')`})
+//         }
+//         //console.log(`Required fields missing ${Object.keys(error.errors)}`)
+//         //return response.status(400).send({error: `Required fields missing ${Object.keys(error.errors)}`})
+//         return response.status(400).json({ error: error.message })
+//     } else if (error.name === 'JsonWebTokenError') {
+//         if (error.message === 'jwt must be provided') {
+//             logger.error(`Error: ${error.name}: ${error.message}`)
+//             return response.status(400).send({ error: 'The token must be provided in the header.' })
+//         }
+//     }
     
 
-    next(error)
-}
+//     next(error)
+// }
 
-app.use(errorHandler)
+app.use(middleware.errorHandler)
 
 module.exports = app
---
+++ utils/middleware.js
@@ -0,0 +1,60 @@
+// kopioidaan notes-backendin middlewaret aluksi
+const logger = require('./logger')
+
+const requestLogger = (request, response, next) => {
+    logger.info('Method:', request.method)
+    logger.info('Path:  ', request.path)
+    logger.info('Body   ', request.body)
+    logger.info(Date.now())
+    logger.info('---')
+    next()
+}
+
+// const unknownEndpoint = (request, response) => {
+//     response.status(404).send({ error: 'unknown endpoint '})
+// }
+
+
+const errorHandler = (error,request,response,next) => {
+    logger.error(error)
+    logger.error(error.message)
+
+    //logger.error(error)
+    if (error.name === 'CastError') { // PUT
+        return response.status(400).send({ error: `Error while updating likes field: likes must be a number`})
+    } else if (error.name === 'ValidationError') {
+        // https://mongoosejs.com/docs/validation.html#validation-errors
+        //console.log(error.errors)
+        if (error.errors.likes) {
+            //logger.error(error.errors.likes.name) // CastError
+            logger.error(error.errors.likes.message)
+            return response.status(400).send({error: `likes field must be a number`})
+        } else if (error.errors.url) {
+            logger.error(error.errors.url.message)
+            return response.status(400).send({error: `url field can't be empty`})
+        } else if (error.errors.title) {
+            logger.error(error.errors.title.message)
+            return response.status(400).send({error: `title field can't be empty`})
+        } else if (error.errors.username.message === 'Username must have at least 3 characters') {
+            logger.error(error.errors.username.message)
+            return response.status(400).send({ error: `Username must have at least 3 characters (given username was: '${error.errors.username.value}')`})
+        }
+        //console.log(`Required fields missing ${Object.keys(error.errors)}`)
+        //return response.status(400).send({error: `Required fields missing ${Object.keys(error.errors)}`})
+        return response.status(400).json({ error: error.message })
+    } else if (error.name === 'JsonWebTokenError') {
+        if (error.message === 'jwt must be provided') {
+            logger.error(`Error: ${error.name}: ${error.message}`)
+            //return response.status(400).send({ error: 'The token must be provided in the header.' })
+            return response.status(400).send({ error: 'token missing or invalid' })
+            }
+        }
+    
+    next(error)
+}
+
+module.exports = {
+    requestLogger,
+    //unknownEndpoint,
+    errorHandler
+}
