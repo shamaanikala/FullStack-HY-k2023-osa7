9da2aafb7f36aa596c8325804a7b5ab889c91a76
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-15 16:24:35 +0300

    T4.22* valmis

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,4 @@
 const blogsRouter = require('express').Router()
-//const blog = require('../models/blog')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
@@ -14,19 +13,7 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', userExtractor, async (request, response) => {
-    //const blog = new Blog(request.body)
     const body = request.body
-    //const decodedToken = jwt.verify(request.token, process.env.SECRET)
-    
-    
-    // tämä varmaan siirtyne middlewareen T4.20*
-    // mutta miten tämän voisi lähettää errorHandler?
-    // if (!decodedToken.id) {
-    //     return response.status(401).json({ error: 'token invalid' })
-    // }
-    
-    //const user = await User.findById(body.userId)
-    //const user = await User.findById(decodedToken.id)
     const user = request.user
 
     const blog = new Blog({
@@ -45,12 +32,6 @@ blogsRouter.post('/', userExtractor, async (request, response) => {
 })
 
 blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-    // const decodedToken = jwt.verify(request.token, process.env.SECRET)
-
-    // if (!decodedToken.id) {
-    //     return response.status(401).json({ error: 'token invalid' })
-    // }
-
     const blogId = request.params.id
     const blogToDelete = await Blog.findById(blogId)
 
@@ -62,7 +43,6 @@ blogsRouter.delete('/:id', userExtractor, async (request, response) => {
     // https://stackoverflow.com/questions/2342579/http-status-code-for-update-and-delete
     if (!blogToDelete) {
         //console.log('Id:llä ei löydy tietokannasta blogia')
-        //console.log(blogToDelete)
         logger.info(`(blogsRouter: delete): ${Date()}`)
         logger.info(`DELETE request done with nonexisting id: ${blogId}`)
         return response.status(404).end()
@@ -73,7 +53,7 @@ blogsRouter.delete('/:id', userExtractor, async (request, response) => {
 
     // jos jostain syystä tietokannasta löytyy blogi ilman
     // user kenttää, heittää palvelin tässä kohtaa silloin 500
-    //const deleterId = decodedToken.id.toString()
+
     const deleter = request.user
     const deleterId = deleter.id
     
@@ -85,13 +65,6 @@ blogsRouter.delete('/:id', userExtractor, async (request, response) => {
         return response.status(500).json({ error: `Unable to finish the DELETE operation: missing user information` })
     }
     const blogToDeleteCreator = blogToDelete.user.toString()
-
-    //const deleterId = decodedToken.id.toString()
-
-    // console.log(`id token: ${decodedToken.id}`)
-    // console.log(`blogToDelete creator id: ${blogToDeleteCreator}`)
-    // console.log(`userid tokenista: ${deleterId}`)
-    // console.log(blogToDelete)
     
     if (blogToDeleteCreator !== deleterId) {
         //console.log('Tokenin käyttäjällä ei ole oikeutta deletoida blogia')
@@ -100,9 +73,7 @@ blogsRouter.delete('/:id', userExtractor, async (request, response) => {
         logger.info(`- from userId ${deleterId}`)
         logger.info(`- to blogId ${blogId} by userId ${blogToDeleteCreator}`)
         return response.status(401).json({ error: 'UserId wrong, cannot delete blog' })
-    } //else {
-        //console.log('Tokenin ja blogin user.id on sama -> poistetaan')
-    //}
+    }
 
     await Blog.findByIdAndRemove(request.params.id)
     response.status(204).end()
@@ -111,18 +82,6 @@ blogsRouter.delete('/:id', userExtractor, async (request, response) => {
 blogsRouter.put('/:id', async (request, response) => {
     const body = request.body
     const { title, author, url, likes } = request.body
-
-    //console.log(request.body)
-    //console.log(body)
-
-    // const blog = {
-    //     title: body.title,
-    //     author: body.author,
-    //     url: body.url,
-    //     likes: body.likes
-    // }
-
-    //console.log(blog)
 
     // tälle annetaan javascript-olio eikä Blog-olio
     // new parametri palauttaa muuttuneen olion kutsujalle
