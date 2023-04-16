d8af70b512364ef8645345fc0d32a5aa6618fe2c
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-16 17:58:02 +0300

    T5.1 login-lomake näkyy jo, mutta ei toimi

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
+  return(
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input 
+        type="text"
+        value={username}
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+          />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    console.log('Login käyntiin')
+  }
+
+
+  return (
+    <div>
+      {!user && <LoginForm 
+        handleLogin={handleLogin}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />}
+      {user && <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+      </div>
+    }
+    </div>
+  )
+}
+
+export default App
