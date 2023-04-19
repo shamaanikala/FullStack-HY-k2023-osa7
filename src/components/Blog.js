776b25e2a72327be0f428451459f689579a7a7cd
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-19 11:52:27 +0300

    T5.7 lisätään blogilistaukseen tyyli ja nimen klik

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,24 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title}
-    <button onClick={() => console.log('blogin view klikattu')}>view</button> 
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  // klikattavan tekstin idea tämän perusteella:
+  // https://stackoverflow.com/questions/43630991/by-clicking-text-how-to-change-clicked-text-to-another-text-in-react-js
+  // eli luultavasti melkein mihin tahansa tagiin voi lisätä onClick()
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        <span onClick={() => console.log('blogin nimeä klikattu')}>{blog.title}</span>
+          <button onClick={() => console.log('blogin view klikattu')}>view</button> 
+      </div>
+    </div>
+  )
+}
 
 export default Blog
