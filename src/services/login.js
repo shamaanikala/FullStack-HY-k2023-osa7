64f7a836abc4897c606882ee0b961ac0214dd0df
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-24 17:30:03 +0300

    T5.13 ESLint juttu pois

--- src/services/login.js
+++ src/services/login.js
@@ -14,5 +14,5 @@ const verify = async (userWithToken,config) => {
   return response.data
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
+//// eslint-disable-next-line import/no-anonymous-default-export
 export default { login, verify }
