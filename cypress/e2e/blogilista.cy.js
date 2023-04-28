a5369e0bc4952b813b74e2e730956a65b40ec670
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-28 11:52:59 +0300

    T5.18 valmis, epäonnistuneen kirjautumisen testi

--- cypress/e2e/blogilista.cy.js
+++ cypress/e2e/blogilista.cy.js
@@ -29,7 +29,7 @@ describe('Blog app', function() {
   })
 
   describe('Login',function() {
-    it.only('succeeds with correct credentials', function() {
+    it('succeeds with correct credentials', function() {
       cy.get('#username').type('mmeika')
       cy.get('#password').type('salasana')
       cy.contains('login').click()
@@ -38,8 +38,21 @@ describe('Blog app', function() {
       cy.contains('Matti Meikäläinen logged in')
     })
 
-  //   it('fails with wrong credentials', function() {
-  //     // ...
-  //   })
+    it.only('fails with wrong credentials', function() {
+      cy.get('#username').type('mmeika')
+      cy.get('#password').type('väärä-salasana')
+      cy.contains('login').click()
+
+      //cy.contains('wrong username or password')
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+        .and('have.css', 'border-style', 'solid')
+
+      cy.get('html')
+        .should('not.contain', 'Matti Meikäläinen')
+        .and('not.contain', 'logged in')
+        .and('contain', 'log in to application')
+    })
   })
 })
