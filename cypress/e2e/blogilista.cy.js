39bac3234a71953f9580e5d207973e1eb224472b
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-28 10:44:09 +0300

    T5.17 e2e testin aihio

--- cypress/e2e/blogilista.cy.js
+++ cypress/e2e/blogilista.cy.js
@@ -1,10 +1,20 @@
 describe('Blog app', function() {
-  beforeEach(function() {
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+  it('etusivu näkyy', function () {
     cy.visit('http://localhost:3000')
-  })
+    // blogien ei pitäisi vielä näkyä
+    //cy.contains('blogs') // ei mene läpi
+    cy.get('html') // 'html' ~ näkyvät jutut
+      .should('contain', 'log in to application')
+      .and('not.contain', 'blogs')
 
-  it('Login form is shown', function() {
-    // ...
+    cy.contains('blogs').should('not.exist')
   })
+  // beforeEach(function() {
+  //   cy.request('POST', 'http://localhost:3003/api/testing/reset')
+  //   cy.visit('http://localhost:3000')
+  // })
+
+  // it('Login form is shown', function() {
+  //   // ...
+  // })
 })
