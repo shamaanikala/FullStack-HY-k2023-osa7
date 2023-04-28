914dbe5cdcace302ad84fff90e87c8b342106164
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-04-28 11:15:14 +0300

    T5.17 valmis

--- cypress/e2e/blogilista.cy.js
+++ cypress/e2e/blogilista.cy.js
@@ -1,20 +1,24 @@
 describe('Blog app', function() {
-  it('etusivu näkyy', function () {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
-    // blogien ei pitäisi vielä näkyä
-    //cy.contains('blogs') // ei mene läpi
+  })
+
+  it('Login form is shown', function() {
     cy.get('html') // 'html' ~ näkyvät jutut
       .should('contain', 'log in to application')
       .and('not.contain', 'blogs')
 
     cy.contains('blogs').should('not.exist')
-  })
-  // beforeEach(function() {
-  //   cy.request('POST', 'http://localhost:3003/api/testing/reset')
-  //   cy.visit('http://localhost:3000')
-  // })
 
-  // it('Login form is shown', function() {
-  //   // ...
-  // })
+    // löytyykö itse lomake
+    cy.get('form')
+      .contains('username')
+    cy.get('form')
+      .should('contain', 'password')
+
+    cy.get('form')
+      .get('button')
+      .should('have.text', 'login')
+  })
 })
