f0140a6fd8a782b2dbe1bf04cdad64ae06ca1249
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-10 16:18:38 +0300

    T7.1 valmis

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
-  Link,
-  Navigate
+  Link
 } from 'react-router-dom'
 
 const Menu = () => {
@@ -20,11 +18,6 @@ const Menu = () => {
     </div>
   )
 }
-
-{/* <a href='#' style={padding}>anecdotes</a>
-<a href='#' style={padding}>create new</a>
-<a href='#' style={padding}>about</a> */}
-
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
