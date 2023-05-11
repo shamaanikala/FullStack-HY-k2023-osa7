720d6c9c22c71c01bedb26f252f714ebe7405800
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-11 18:40:25 +0300

    T7.8 tiedot haetaan palvelimelta hookissa

--- ultimate-hooks/src/App.js
+++ ultimate-hooks/src/App.js
@@ -18,7 +18,12 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(response => {
+        setResources(response.data)
+      })
+  },[baseUrl])
 
   const create = (resource) => {
     // ...
