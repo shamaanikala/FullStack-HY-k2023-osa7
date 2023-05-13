8e81261581b6625740b3993fc6e866316a354d7a
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-13 19:56:21 +0300

    T7.10 päivitetään eslintrc switch-case sääntö

--- bloglist-frontend-redux/.eslintrc.js
+++ bloglist-frontend-redux/.eslintrc.js
@@ -22,7 +22,10 @@ module.exports = {
     "rules": {
         "indent": [
             "error",
-            2  
+            2,
+            {
+                "SwitchCase": 1
+            }  
         ],
         "linebreak-style": [
             "error",
