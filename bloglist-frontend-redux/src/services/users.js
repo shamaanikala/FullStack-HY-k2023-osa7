14bf0b5c64acb60b87b5d5bf546d8da9e5e18b46
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-16 14:54:50 +0300

    T7.14 aihio users servicelle

---
+++ bloglist-frontend-redux/src/services/users.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+const baseUrl = '/api/users'
+
+// ei tehdä yleistä palvelu-komponenttia, koska tähän riittää vain get
+
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  console.log(response)
+  console.log(response.data)
+  return response.data
+}
+
+export default { getAll }
