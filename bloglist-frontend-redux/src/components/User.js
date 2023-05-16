1cd4e6b5aef050440ae3b090f03742562b4dfaf9
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-16 17:44:33 +0300

    T7.15 valmis

--- bloglist-frontend-redux/src/components/User.js
+++ bloglist-frontend-redux/src/components/User.js
@@ -2,13 +2,29 @@ import { useParams } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import usersService from '../services/users'
 
+const BlogTitleList = ({ blogs }) => {
+  return (
+    <>
+      <ul>
+        {blogs.map(blog => (
+          <li key={blog.title}>{blog.title}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
 const User = () => {
   const id = useParams().id
   const result = useQuery(
     'users',
     () => usersService.getAll().then(res => res) // getAll antaa .data
   )
-  console.log(result)
+
+  // User sivun uudelleenlatausbugin välttäminen:
+  if (!result.data) {
+    return null
+  }
   const user = result.data.find(user => user.id === id)
 
   return (
@@ -16,9 +32,8 @@ const User = () => {
       <h2>{user.name}</h2>
       <h3>added blogs</h3>
       <div>
-        <ul>
-          <li>Blog title</li>
-        </ul>
+        {user.blogs.length === 0 && <em>No blogs added</em>}
+        {user.blogs.length > 0 && <BlogTitleList blogs={user.blogs} />}
       </div>
     </div>
   )
