c9c71763f57b37adbe03b6f4eac7f353bd15701b
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-16 12:30:44 +0300

    T7.14 Users komponentti

--- bloglist-frontend-redux/src/App.js
+++ bloglist-frontend-redux/src/App.js
@@ -14,6 +14,7 @@ import { setUser } from './reducers/userReducer'
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Blogs from './components/Blogs'
+import Users from './components/Users'
 
 const App = () => {
   const [username, setUsername] = useState('')
@@ -117,14 +118,7 @@ const App = () => {
             </p>
             <Routes>
               <Route path="/" element={<Blogs user={user} />} />
-              <Route
-                path="/users"
-                element={
-                  <div>
-                    <h2>Users</h2>😸
-                  </div>
-                }
-              />
+              <Route path="/users" element={<Users />} />
             </Routes>
           </div>
         )}
