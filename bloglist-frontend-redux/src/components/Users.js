d3ceb849cb01c68a64d345e42c0e08ad393f46c3
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-16 15:17:10 +0300

    T7.14 aihio Users sivun taulukolle

--- bloglist-frontend-redux/src/components/Users.js
+++ bloglist-frontend-redux/src/components/Users.js
@@ -1,7 +1,35 @@
+// import usersService from '../services/users'
+
+// const getUserData = async () => await usersService.getAll()
+
 const Users = () => {
+  // dummy dataa
+  const userData = [
+    { name: 'Arto Hellas', aggregatedBlogNumber: 6 },
+    { name: 'Matti Luukkainen', aggregatedBlogNumber: 2 },
+    { name: 'Venla Ruuska', aggregatedBlogNumber: 0 },
+  ]
+
   return (
     <div>
-      <h2>Users</h2>😸
+      <h2>Users 😸</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td></td>
+            <td>
+              <b>blogs created</b>
+            </td>
+          </tr>
+          {userData &&
+            userData.map(user => (
+              <tr key={user.name}>
+                <td>{user.name}</td>
+                <td>{user.aggregatedBlogNumber}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   )
 }
