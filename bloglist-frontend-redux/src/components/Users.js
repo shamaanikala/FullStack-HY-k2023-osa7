f0cab7bab8be05d666e9867ec64980885590e918
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-16 15:43:12 +0300

    T7.14 haetaan Users-taulukon tiedot reactQueryllä

--- bloglist-frontend-redux/src/components/Users.js
+++ bloglist-frontend-redux/src/components/Users.js
@@ -1,35 +1,42 @@
-// import usersService from '../services/users'
-
-// const getUserData = async () => await usersService.getAll()
+import { useQuery } from 'react-query'
+import usersService from '../services/users'
 
 const Users = () => {
   // dummy dataa
-  const userData = [
-    { name: 'Arto Hellas', aggregatedBlogNumber: 6 },
-    { name: 'Matti Luukkainen', aggregatedBlogNumber: 2 },
-    { name: 'Venla Ruuska', aggregatedBlogNumber: 0 },
-  ]
+  // const userData = [
+  //   { name: 'Arto Hellas', aggregatedBlogNumber: 6 },
+  //   { name: 'Matti Luukkainen', aggregatedBlogNumber: 2 },
+  //   { name: 'Venla Ruuska', aggregatedBlogNumber: 0 },
+  // ]
+
+  const result = useQuery(
+    'users',
+    () => usersService.getAll().then(res => res) // getAll antaa .data
+  )
+  console.log(result)
 
   return (
     <div>
       <h2>Users 😸</h2>
-      <table>
-        <tbody>
-          <tr>
-            <td></td>
-            <td>
-              <b>blogs created</b>
-            </td>
-          </tr>
-          {userData &&
-            userData.map(user => (
-              <tr key={user.name}>
+      {result.isLoading && <div>loading...</div>}
+      {result.isSuccess && (
+        <table>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>
+                <b>blogs created</b>
+              </td>
+            </tr>
+            {result.data.map(user => (
+              <tr key={user.id}>
                 <td>{user.name}</td>
-                <td>{user.aggregatedBlogNumber}</td>
+                <td>{user.blogs.length}</td>
               </tr>
             ))}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
--- bloglist-frontend-redux/src/index.js
+++ bloglist-frontend-redux/src/index.js
@@ -5,8 +5,14 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './store'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </Provider>
 )
