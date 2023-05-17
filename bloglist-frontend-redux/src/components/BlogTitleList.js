b877176af2278cb2de301b7312741e2985160674
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-17 20:06:00 +0300

    T7.17 valmis, lisätään käyttäjän blogeihin linkit

--- bloglist-frontend-redux/src/components/BlogTitleList.js
+++ bloglist-frontend-redux/src/components/BlogTitleList.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 const BlogTitleList = ({ blogs }) => {
   return (
     <>
       <ul>
         {blogs.map(blog => (
-          <li key={blog.title}>{blog.title}</li>
+          <li key={blog.title}>
+            <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+          </li>
         ))}
       </ul>
     </>
