1012a203d0b7eca25431bf3f453fd26cc41756ed
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-17 21:03:54 +0300

    T7.18 kommentin lähettäminen palvelimelle

--- bloglist-backend/controllers/comments.js
+++ bloglist-backend/controllers/comments.js
@@ -4,10 +4,24 @@ const Blog = require('../models/blog')
 
 const { requestLogger } = require('../utils/middleware')
 const logger = require('../utils/logger')
+const blog = require('../models/blog')
 
 commentsRouter.get('/', requestLogger, async (request, response) => {
-  const comments = await Comment.find({}).populate('blog', { _id: 1 })
+  const comments = await Comment.find({}).populate('blog', { title: 1 })
   response.json(comments)
+})
+
+commentsRouter.post('/', requestLogger, async (request, response) => {
+  const body = request.body
+
+  const comment = new Comment({
+    content: body.content,
+    blog: body.blog,
+  })
+
+  const savedComment = await comment.save()
+
+  response.status(201).json(savedComment)
 })
 
 module.exports = commentsRouter
