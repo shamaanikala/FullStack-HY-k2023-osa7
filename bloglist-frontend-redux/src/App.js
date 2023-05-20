0e17d2e9bf6a0647cacddda90bab681d9dd42ed8
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-20 10:13:38 +0300

    T7.20 poistetaan huono <Snackbar>

--- bloglist-frontend-redux/src/App.js
+++ bloglist-frontend-redux/src/App.js
@@ -8,7 +8,6 @@ import AppRouter from './components/AppRouter'
 import { useQueryClient } from 'react-query'
 import { Page } from './styles'
 import { Container } from '@mui/material'
-import SimpleSnackbar from './MUI_components/snackbars'
 
 const App = () => {
   const user = useSelector(state => state.user)
@@ -32,7 +31,6 @@ const App = () => {
             <AppRouter />
           </div>
         )}
-        <SimpleSnackbar />
       </Page>
     </Container>
   )
