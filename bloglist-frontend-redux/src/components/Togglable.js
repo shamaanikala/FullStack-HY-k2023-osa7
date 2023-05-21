1038c708299f4e77edb3eeb2377e90428875cbe0
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-21 10:43:25 +0300

    T7.21 Togglable käyttää MUI-nappuloita

--- bloglist-frontend-redux/src/components/Togglable.js
+++ bloglist-frontend-redux/src/components/Togglable.js
@@ -1,6 +1,7 @@
 // Osan 5 materiaalista
 import { useState, useImperativeHandle, forwardRef } from 'react'
 import PropTypes from 'prop-types'
+import { Button } from '@mui/material'
 
 const Togglable = forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false)
@@ -35,11 +36,15 @@ const Togglable = forwardRef((props, ref) => {
   return (
     <div>
       <div style={hideWhenVisible}>
-        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+        <Button variant="contained" size="small" onClick={toggleVisibility}>
+          {props.buttonLabel}
+        </Button>
       </div>
       <div style={showWhenVisible}>
         {props.children}
-        <button onClick={toggleVisibility}>cancel</button>
+        <Button variant="contained" size="small" onClick={toggleVisibility}>
+          cancel
+        </Button>
       </div>
     </div>
   )
