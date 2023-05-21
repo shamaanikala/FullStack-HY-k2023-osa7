eea75c00c367af1febdca6d7a426a3dfc60b18d7
V. Katajisto <18308619+shamaanikala@users.noreply.github.com>
2023-05-21 14:42:33 +0300

    T7.21 login MUI lomake ja <Card>

--- bloglist-frontend-redux/src/components/LoginForm.js
+++ bloglist-frontend-redux/src/components/LoginForm.js
@@ -1,35 +1,36 @@
-const LoginForm = ({
-  handleLogin,
-  username,
-  setUsername,
-  password,
-  setPassword,
-}) => {
+import { TextField, Card, CardContent, CardActions, Button } from '@mui/material'
+
+const LoginForm = ({ handleLogin, username, setUsername, password, setPassword }) => {
   return (
     <>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            id="username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            id="password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Card sx={{ width: 'fit-content' }}>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            <div>
+              <TextField
+                label="Username"
+                value={username}
+                name="Username"
+                id="username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              <TextField
+                label="Password"
+                type="password"
+                value={password}
+                name="Password"
+                id="password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <CardActions>
+              <Button type="submit">login</Button>
+            </CardActions>
+          </form>
+        </CardContent>
+      </Card>
     </>
   )
 }
